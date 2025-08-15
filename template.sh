fd5aeb7235c7d1956ffe3d5fd3a4682449759c23
aryarautt <rautarya2004@gmail.com>
2025-08-16 01:07:23 +0530

    Add deployment script and CI workflow

---
+++ template.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+yum update -y
+yum install -y httpd git
+systemctl start httpd
+systemctl enable httpd
+usermod -aG apache ec2-user
+chmod 755 /var/www/html
+cd /var/www/html
+git clone https://github.com/aryarautt/arya-aws-static-website.git
+cp -r arya-aws-static-website/* /var/www/html/
+echo "<h1>Hello from $(hostname -f) webserver</h1>" >> /var/www/html/index.html
